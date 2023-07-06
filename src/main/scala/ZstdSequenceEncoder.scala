6d2533777c3aefb5207ad02ecb6a850fce281ded
Sagar Karandikar <sagark@eecs.berkeley.edu>
2023-07-06 06:03:02 -0700

    fix import for cde from bumped rocket-chip

--- src/main/scala/ZstdSequenceEncoder.scala
+++ src/main/scala/ZstdSequenceEncoder.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import chisel3.{Printable}
 import freechips.rocketchip.tile._
-import freechips.rocketchip.config._
+import org.chipsalliance.cde.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.rocket.{TLBConfig, HellaCacheArbiter}
 import freechips.rocketchip.util.DecoupledHelper
